a02b199d4e75532850067b63e58941b041ce65e1
Oron Ben Zvi <oronbz@gmail.com>
2026-02-24 16:08:11 +0200

    Initial Commit

---
+++ CouseBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CouseBar
+//
+//  Created by Oron Ben Zvi on 24/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CouseBar/CouseBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouseBarApp.swift
+//  CouseBar
+//
+//  Created by Oron Ben Zvi on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CouseBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
